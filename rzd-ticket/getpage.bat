e521966266bfb85a8ac87dbe66c65dcbbb65b639
Sergio <sergvcx@mail.ru>
2014-12-25 18:14:07 +0400

    commit from note

--- rzd-ticket/getpage.bat
+++ rzd-ticket/getpage.bat
@@ -1,1 +1,1 @@
-wget  --proxy-user=mushkaev --proxy-password=sergpasw --output-file=info.txt --output-document=utfpage.txt  --input-file=%1
+wget   --output-file=info.txt --output-document=utfpage.txt  --input-file=%1
