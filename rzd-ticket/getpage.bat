999b16bcaf12501fb4a738d5ffbb0967ca343667
mushkaev <mushkaev@module.ru>
2014-12-25 17:50:44 +0300

    scanner

---
+++ rzd-ticket/getpage.bat
@@ -0,0 +1,1 @@
+wget  --proxy-user=mushkaev --proxy-password=sergpasw --output-file=info.txt --output-document=utfpage.txt  --input-file=%1
---
+++ rzd-ticket/utf2win.bat
@@ -0,0 +1,1 @@
+win_iconv.exe -f UTF-8 -t WINDOWS-1251 utfpage.txt > winpage.txt
