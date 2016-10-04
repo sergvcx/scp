3979747aee213a76a5d645b0c57dc2b8baca91a5
sergvcx <sergvcx@mail.ru>
2016-10-04 22:37:11 +0300

    metro

---
+++ phantom/metro.js
@@ -0,0 +1,54 @@
+phantom.injectJs('settings.js');
+var casper = require('casper').create();
+var fs = require('fs');
+casper.userAgent('Mozilla/5.0 (Macintosh; Intel Mac OS X)');
+
+
+//casper.start('http://ya.ru', function() {
+casper.start(metro, function() {
+	phantom.outputEncoding="cp866";
+    this.echo(this.getTitle());
+	this.capture('metro0.png');
+});
+
+
+
+casper.then(function() {
+	
+	
+	this.capture('metro0.png');
+	fs.write('metro0.html', this.getHTML() , 'w');
+	
+	//	<a href="#" id="btnStart" class="preloader"></a>
+	// <button id="btnDefault" type="submit" class="btn">Войти в Интернет</button>
+	//this.waitForSelector('a.preloader', function(){
+	this.waitForSelector('button.btn', function(){
+		this.capture('metro1.png');
+		//this.click('a.preloader');
+		this.click('button.btn');
+		this.capture('metro2.png');
+		this.echo('Войти');
+		fs.write('metro1.html', this.getHTML() , 'w');
+	},function(){
+		this.echo('ERROR:No selector');
+	}
+	,10000);
+	
+	this.then(function(){
+		this.echo('Wellcome');
+		this.capture('metro2.png');
+		fs.write('metro2.html', this.getHTML() , 'w');
+		this.wait(3000,function(){
+			this.capture('metro3.png');
+			fs.write('metro3.html', this.getHTML() , 'w');
+		});
+	});
+	
+	
+	
+	
+},10000);
+
+
+
+casper.run();
